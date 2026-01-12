462921482c28eeb79b3050874a09f5ba6ec91cbe
amonnoir5-rgb <amonnoir5@gmail.com>
2026-01-12 09:54:06 +0200

    Add files via upload

---
+++ node.js
@@ -0,0 +1,11 @@
+function show(){
+    document.getElementById("menu").style.display ="block";
+    document.getElementById("btn2").style.display ="block";
+    document.getElementById("btn3").style.display ="none";
+    document.getElementById("input").style.marginLeft ="-40px";
+}
+function hide(){
+    document.getElementById("menu").style.display ="none";
+    document.getElementById("btn3").style.display ="block";
+    document.getElementById("input").style.marginLeft ="-20px";
+}
